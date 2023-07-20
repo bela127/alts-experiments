0572b0c0f6ce42694fc20bea365bfd7ffe74533d
bela127 <bhb127@outlook.de>
2023-07-20 12:55:51 +0200

    datasources_tests.py new interface

--- alts/experiments/datasources_tests.py
+++ alts/experiments/datasources_tests.py
@@ -1,13 +1,11 @@
 
-from alts.modules.query.query_sampler import LatinHypercubeQuerySampler
 from alts.modules.blueprint import BaselineBlueprint
 from alts.core.oracle.data_source import DataSource
 from alts.modules.data_process.process import DataSourceProcess
-from alts.modules.oracle.query_queue import FCFSQueryQueue
 from alts.modules.oracle.data_source import LineDataSource
 from alts.modules.stopping_criteria import TimeStoppingCriteria
 from alts.modules.query.query_sampler import LatinHypercubeQuerySampler, UniformQuerySampler
-from alts.core.experiment_modules import ExperimentModules
+from alts.core.experiment_modules import InitQueryExperimentModules
 from alts.core.query.query_selector import ResultQuerySelector
 from alts.modules.query.query_optimizer import NoQueryOptimizer
 from alts.modules.query.query_decider import AllQueryDecider
@@ -26,7 +24,6 @@ from alts.modules.oracle.data_source import (
     StarDataSource,
     GaussianProcessDataSource,
     )
-from alts.core.experiment_modules import ExperimentModules
 
 dss = [
     LineDataSource,
@@ -54,7 +51,8 @@ def create_blueprints():
                 data_source=ds(query_shape = (1,), result_shape = (1,))
             ),
 
-            experiment_modules = ExperimentModules(
+            experiment_modules = InitQueryExperimentModules(
+                initial_query_sampler = LatinHypercubeQuerySampler(num_queries=1),
                 query_selector=ResultQuerySelector(
                     query_optimizer=NoQueryOptimizer(query_sampler=LatinHypercubeQuerySampler()),
                     query_decider=AllQueryDecider(),
@@ -62,7 +60,6 @@ def create_blueprints():
                 ),
 
             stopping_criteria = TimeStoppingCriteria(stop_time=21),
-            initial_query_sampler = LatinHypercubeQuerySampler(num_queries=1),
             exp_name=f"{ds.__name__}", # type: ignore
         )
 
